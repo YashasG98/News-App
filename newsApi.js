6ac210a2193f70a9965456feac2e037b3436ce7c
YashasG98 <yashu191298@gmail.com>
2019-05-03 18:14:05 +0530

    Added essential files

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '4ff2fc7043244cc68786419c4a42ff48';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=in&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
